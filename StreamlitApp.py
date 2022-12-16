55b0dc20e127add2b5301e6fe55a7d9e84b4f405
tuanhung1405 <120279152+tuanhung1405@users.noreply.github.com>
2022-12-16 15:47:30 +0700

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import ApiGetData
-import ta
-from ArimaModel import ArimaModel
 from io import StringIO
 import sys
-import plotly.express as px
 from PIL import Image
 
 st.set_page_config(page_title='Data Analyst Portfolio Project – Sales Management',page_icon="📶",layout="wide")

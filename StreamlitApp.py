d1aa00f565bbc291e95d4a35cd149a87c48e4d4c
tuanhung1405 <120279152+tuanhung1405@users.noreply.github.com>
2022-12-16 16:09:25 +0700

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -72,8 +72,8 @@ One data source (sales budgets) were provided in Excel format and were connected
 
 Below are the SQL statements for cleansing and transforming necessary data.
 """)
-st.subheader('DIM_Calendar:')
 
+st.subheader('DIM_Calendar:')
 code = '''-- Cleansed DIM_Date Table --
 SELECT 
   [DateKey], 
@@ -100,6 +100,148 @@ FROM
 WHERE 
   CalendarYear >= 2019'''
 st.code(code, language='sql')
+
+st.subheader('DIM_Customers:')
+code = '''-- Cleansed DIM_Customers Table --
+SELECT 
+  c.customerkey AS CustomerKey, 
+  --      ,[GeographyKey]
+  --      ,[CustomerAlternateKey]
+  --      ,[Title]
+  c.firstname AS [First Name], 
+  --      ,[MiddleName]
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  -- Combined First and Last Name
+  --      ,[NameStyle]
+  --      ,[BirthDate]
+  --      ,[MaritalStatus]
+  --      ,[Suffix]
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  --      ,[EmailAddress]
+  --      ,[YearlyIncome]
+  --      ,[TotalChildren]
+  --      ,[NumberChildrenAtHome]
+  --      ,[EnglishEducation]
+  --      ,[SpanishEducation]
+  --      ,[FrenchEducation]
+  --      ,[EnglishOccupation]
+  --      ,[SpanishOccupation]
+  --      ,[FrenchOccupation]
+  --      ,[HouseOwnerFlag]
+  --      ,[NumberCarsOwned]
+  --      ,[AddressLine1]
+  --      ,[AddressLine2]
+  --      ,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+  --      ,[CommuteDistance]
+  g.city AS [Customer City] -- Joined in Customer City from Geography Table
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC -- Ordered List by CustomerKey'''
+st.code(code, language='sql')
+
+st.subheader('DIM_Products:')
+code = '''-- Cleansed DIM_Products Table --
+SELECT 
+  p.[ProductKey], 
+  p.[ProductAlternateKey] AS ProductItemCode, 
+  --      ,[ProductSubcategoryKey], 
+  --      ,[WeightUnitMeasureCode]
+  --      ,[SizeUnitMeasureCode] 
+  p.[EnglishProductName] AS [Product Name], 
+  ps.EnglishProductSubcategoryName AS [Sub Category], -- Joined in from Sub Category Table
+  pc.EnglishProductCategoryName AS [Product Category], -- Joined in from Category Table
+  --      ,[SpanishProductName]
+  --      ,[FrenchProductName]
+  --      ,[StandardCost]
+  --      ,[FinishedGoodsFlag] 
+  p.[Color] AS [Product Color], 
+  --      ,[SafetyStockLevel]
+  --      ,[ReorderPoint]
+  --      ,[ListPrice] 
+  p.[Size] AS [Product Size], 
+  --      ,[SizeRange]
+  --      ,[Weight]
+  --      ,[DaysToManufacture]
+  p.[ProductLine] AS [Product Line], 
+  --     ,[DealerPrice]
+  --      ,[Class]
+  --      ,[Style] 
+  p.[ModelName] AS [Product Model Name], 
+  --      ,[LargePhoto]
+  p.[EnglishDescription] AS [Product Description], 
+  --      ,[FrenchDescription]
+  --      ,[ChineseDescription]
+  --      ,[ArabicDescription]
+  --      ,[HebrewDescription]
+  --      ,[ThaiDescription]
+  --      ,[GermanDescription]
+  --      ,[JapaneseDescription]
+  --      ,[TurkishDescription]
+  --      ,[StartDate], 
+  --      ,[EndDate], 
+  ISNULL (p.Status, 'Outdated') AS [Product Status] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] as p
+  LEFT JOIN dbo.DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
+  LEFT JOIN dbo.DimProductCategory AS pc ON ps.ProductCategoryKey = pc.ProductCategoryKey 
+order by 
+  p.ProductKey asc'''
+st.code(code, language='sql')
+
+st.subheader('FACT_InternetSales:')
+code = '''-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Ensures we always only bring two years of date from extraction.
+ORDER BY
+  OrderDateKey ASC'''
+st.code(code, language='sql')
+st.write('---')
+
+
+st.header('Data Model')
+st.write("""Below is a screenshot of the data model after cleansed and prepared tables were read into Power BI.
+
+This data model also shows how FACT_Budget hsa been connected to FACT_InternetSales and other necessary DIM tables.""")
+st.write('---')
+
+
+st.header('Sales Management Dashboard')
+st.write("""The finished sales management dashboard with one page with works as a dashboard and overview, with two other pages focused on combining tables for necessary details and visualizations to show sales over time, per customers and per products.""")
+st.write("**Click the picture to to open the dashboard and try it out!**")
+
+
 
                    
 
